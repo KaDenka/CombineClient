a0af2fc072611cb89a0bd0dff697eb56a0aa4c54
Denis <recmanus@gmail.com>
2022-02-16 11:45:39 +0300

    Lesson5_Stage2
    Fixing GamblingViewController and preparing GamblingViewModel

--- CombineClient/CombineClient/Controller/GamblingViewController.swift
+++ CombineClient/CombineClient/Controller/GamblingViewController.swift
@@ -12,22 +12,13 @@ class GamblingViewController: UIViewController {
     @IBOutlet weak var emojiOne: UILabel!
     @IBOutlet weak var emojiTwo: UILabel!
     @IBOutlet weak var emojiThree: UILabel!
+    @IBOutlet weak var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+       
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
